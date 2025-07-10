a417c75346557e22b09d580f60cb8f919de26da7
ron12341 <arvincruz7524@gmail.com>
2025-07-10 13:30:11 -0500

    minor changes

--- frontend/web/src/services/admin/productService.js
+++ frontend/web/src/services/admin/productService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_API_URL + "/admin/products";
-const token = JSON.parse(localStorage.getItem("user")).token;
+const token = JSON.parse(localStorage.getItem("user"))?.token;
 
 const addProduct = async ({ name, query, imageUrl, stores }) => {
   try {
