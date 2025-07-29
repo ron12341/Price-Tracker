e1fb694a9e34179180a53d6d9aee6ad0e5c21b9e
ron12341 <arvincruz7524@gmail.com>
2025-07-29 14:49:32 -0500

    added scraping support to canada computers e-commerce

--- backend/api-python/main.py
+++ backend/api-python/main.py
@@ -4,6 +4,8 @@ from models import ScrapeRequest, ScrapeResponse
 
 from scrapers.scrape_amazon import scrape_amazon
 from scrapers.scrape_walmart import scrape_walmart
+from scrapers.scrape_newegg import scrape_newegg
+from scrapers.scrape_canada_computers import scrape_canada_computers
 
 app = FastAPI()
 
@@ -17,9 +19,13 @@ async def scrape(request: ScrapeRequest):
         price, img = "N/A", None
 
         if storeName == "amazon":
-            price, img = price, img = scrape_amazon(store.url, imageUrl)
+            price, img = scrape_amazon(store.url, imageUrl)
         elif storeName == "walmart":
-            price, img = price, img = scrape_walmart(store.url, imageUrl)
+            price, img = scrape_walmart(store.url, imageUrl)
+        elif storeName == "newegg":
+            price, img = scrape_newegg(store.url, imageUrl)
+        elif storeName == "canada computers" or storeName == "canadacomputers":
+            price, img = scrape_canada_computers(store.url, imageUrl)
         elif storeName == "ebay":
             price, img = "N/A", None
 
